6a328a8b9ccfc7302fbdb12cdd62c2704e5a12a7
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-07-15 18:00:21 -0500

    Se puede agregar y eliminar notas en el DOM
    Aun no se usa LocalStorage

--- scripts.js
+++ scripts.js
@@ -1,8 +1,48 @@
+//Variables
+
+let note = document.querySelector('.dataIn')
 let button = document.querySelector('.sendButton')
-let dataIn = document.querySelector('.dataIn')
+let notas = document.querySelector('.notas_container')
 
-button.addEventListener('click',imprimir);
 
-function imprimir(){
-    console.log(`Tu nota es: ${dataIn.value}`)
+//Event Listener
+
+eventListeners()
+
+function eventListeners() {
+
+    button.addEventListener('click',printNote)
+
+    notas.addEventListener('click',deleteNote)
+
 }
+
+
+//Funciones
+
+function imprimirValue() {
+    console.log(note.value);
+}
+
+function printNote() {
+    let noteLi = document.createElement('li')
+    noteLi.classList = "nota"
+    noteLi.innerText = note.value
+
+    let deleteNoteX = document.createElement('a')
+    deleteNoteX.innerText = "X"
+    deleteNoteX.classList = "delete-nota"
+
+    noteLi.appendChild(deleteNoteX)
+    notas.appendChild(noteLi)
+}
+
+function deleteNote(e){
+
+    console.log(e.target.classList);
+    if (e.target.className === 'delete-nota') {
+        e.target.parentElement.remove()
+    }
+
+}
+    
